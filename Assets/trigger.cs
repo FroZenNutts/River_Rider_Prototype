fd8e5970f62807e42352c5728d5db990a10ff0ac
FroZen nutts <prakashbohara71998@gmail.com>
2024-03-30 22:03:40 +0530

    Prototype.......

---
+++ Assets/trigger.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trigger : MonoBehaviour
+{
+    public GameObject enviormentTrigger;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("trigger"))
+        {
+            Debug.Log("Trigger event detected");
+            Instantiate(enviormentTrigger, new Vector3(-44f, 1.21f, 250f), Quaternion.identity);
+        }
+        else
+        {
+            Debug.Log("Other object entered trigger but does not have the 'trigger' tag.");
+        }
+    }
+}
