1baf78154851775ec3bfed354feca38a68ec5401
FroZen nutts <prakashbohara71998@gmail.com>
2024-04-28 15:25:21 +0530

    milestone 6  update

--- Assets/health.cs
+++ Assets/health.cs
@@ -7,33 +7,36 @@ public class health : MonoBehaviour
     public int maxHealth = 100;
     public int currentHealth;
 
-    private void Start()
+    public Healthbar healthBar;
+    public GameObject GameOverPanel;
+
+    // Start is called before the first frame update
+    void Start()
     {
         currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
     }
 
-    private void OnCollisionEnter(Collision collision)
+    // This method should be called when the player takes damage
+    public void TakeDamage(int damage)
     {
+        currentHealth -= damage;
+        // Ensure health doesn't go below 0
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
 
-        if (collision.gameObject.CompareTag("Rocks"))
+        healthBar.SetHealth(currentHealth);
+
+        if (currentHealth <= 0)
         {
-
+            GameOverPanel.SetActive(true);
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Rocks"))
+        {
             TakeDamage(20);
-        }
-
-        void TakeDamage(int damage)
-        {
-            currentHealth -= damage;
-
-            if (currentHealth <= 0)
-            {
-                Die();
-            }
-        }
-
-        void Die()
-        {
-            Debug.Log("Player died!");
+           
         }
     }
 }
